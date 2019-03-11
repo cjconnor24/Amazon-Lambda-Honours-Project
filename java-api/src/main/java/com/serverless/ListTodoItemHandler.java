0d830e394fc312f2562c4148ed89a517ae672592
Chris Connor <chris@chrisconnor.co.uk>
2019-03-11 21:23:28 +0000

    Adding LIST todos endpoint

--- java-api/src/main/java/com/serverless/ListTodoItemHandler.java
+++ java-api/src/main/java/com/serverless/ListTodoItemHandler.java
@@ -1,4 +1,40 @@
 package com.serverless;
 
-public class ListTodoItemHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.DAL.TodoItem;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ListTodoItemHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        try {
+            // get all todolist items
+            List<TodoItem> todoList = new TodoItem().list();
+
+            // send the response back
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(todoList)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        } catch (Exception ex) {
+            logger.error("Error in listing todo list: " + ex);
+
+            // send the error response back
+            Response responseBody = new Response("Error in listing todo list: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
 }
