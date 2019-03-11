27f25c1d435c1b716d87edd0fea1a28b021b81c0
Chris Connor <chris@chrisconnor.co.uk>
2019-03-11 21:11:53 +0000

    Adding the GET single todo item to api

--- java-api/src/main/java/com/serverless/GetTodoItemHandler.java
+++ java-api/src/main/java/com/serverless/GetTodoItemHandler.java
@@ -1,4 +1,52 @@
 package com.serverless;
 
-public class GetTodoItemHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.DAL.TodoItem;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class GetTodoItemHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+        try {
+            // get the 'pathParameters' from input
+            Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+            String todoId = pathParameters.get("id");
+
+            // get the Product by id
+            TodoItem todoItem = new TodoItem().get(todoId);
+
+            // send the response back
+            if (todoItem != null) {
+                return ApiGatewayResponse.builder()
+                        .setStatusCode(200)
+                        .setObjectBody(todoItem)
+                        .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                        .build();
+            } else {
+                return ApiGatewayResponse.builder()
+                        .setStatusCode(404)
+                        .setObjectBody("Todo Item with id: '" + todoId + "' not found.")
+                        .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                        .build();
+            }
+        } catch (Exception ex) {
+            logger.error("Error in retrieving todo item: " + ex);
+
+            // send the error response back
+            Response responseBody = new Response("Error in retrieving todo item: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
 }
