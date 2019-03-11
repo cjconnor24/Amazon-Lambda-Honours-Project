71425e61dc9335270c240ac8ca2de0c3c8e2215d
Chris Connor <chris@chrisconnor.co.uk>
2019-03-11 17:08:12 +0000

    Create Todo is working now :)

--- java-api/src/main/java/com/serverless/CreateTodoItemHandler.java
+++ java-api/src/main/java/com/serverless/CreateTodoItemHandler.java
@@ -1,4 +1,54 @@
 package com.serverless;
 
-public class CreateTodoItemHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.serverless.DAL.TodoItem;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
+public class CreateTodoItemHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private Logger logger = Logger.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+
+        try {
+            // get the 'body' from input
+            JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
+
+
+            TodoItem todoItem = new TodoItem();
+            todoItem.setText(body.get("text").asText());
+            todoItem.setChecked((boolean) body.get("checked").asBoolean());
+            todoItem.setCreatedAt(new Date().getTime());
+            todoItem.setUpdatedAt(new Date().getTime());
+            todoItem.save(todoItem);
+
+            // send the response back
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(todoItem)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+
+        } catch (Exception ex) {
+            logger.error("Error in saving product: " + ex);
+
+            // send the error response back
+            Response responseBody = new Response("Error in saving product: ", input);
+//            Response responseBody = new Response("Error in saving product: ",);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
 }
