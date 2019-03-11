c159772e53ec8d9dd1503fc34974b60bce570036
Chris Connor <chris@chrisconnor.co.uk>
2019-03-11 12:29:31 +0000

    Adding basic TodoList POJO

---
+++ java-api/src/main/java/com/serverless/DAL/TodoItem.java
@@ -0,0 +1,50 @@
+package com.serverless.DAL;
+
+public class TodoItem {
+
+    private String id;
+    private String text;
+    private boolean checked;
+    private int createdAt;
+    private int updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public int getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(int createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(int updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
