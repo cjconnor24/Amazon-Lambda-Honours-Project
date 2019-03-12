788221f14d4de450e377e4730f047c1cac7909a3
Chris Connor <chris@chrisconnor.co.uk>
2019-03-12 11:43:35 +0000

    Getting delete endpoint working

--- java-api/src/main/java/com/serverless/DeleteTodoItemHandler.java
+++ java-api/src/main/java/com/serverless/DeleteTodoItemHandler.java
@@ -1,4 +1,51 @@
 package com.serverless;
 
-public class DeleteTodoItemHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.DAL.TodoItem;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class DeleteTodoItemHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+        try {
+            // get the 'pathParameters' from input
+            Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+            String todoItemId = pathParameters.get("id");
+
+            // get the TodoItem by id
+            Boolean success = new TodoItem().delete(todoItemId);
+
+            // send the response back
+            if (success) {
+                return ApiGatewayResponse.builder()
+                        .setStatusCode(204)
+                        .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                        .build();
+            } else {
+                return ApiGatewayResponse.builder()
+                        .setStatusCode(404)
+                        .setObjectBody("Todo with id: '" + todoItemId + "' not found.")
+                        .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                        .build();
+            }
+        } catch (Exception ex) {
+            logger.error("Error in deleting todo: " + ex);
+
+            // send the error response back
+            Response responseBody = new Response("Error in deleting todo: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
 }
