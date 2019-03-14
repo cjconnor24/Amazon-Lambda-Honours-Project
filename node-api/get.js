de16bf59ccaa492b2c9e92523ba7ee244d523170
Chris Connor <chris@chrisconnor.co.uk>
2019-03-14 13:26:54 +0000

    adding the get and list endpoints along with the config in serverless

---
+++ node-api/get.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.get = (event, context, callback) => {
+
+    const todoID = event.pathParameters.id;
+
+    // TODO: VALIDATE ID
+    // MAKE SURE THERE IS A ID AND IT ISN"T BLANK
+    // if (typeof todoID !== 'string') {
+    //     console.error('Validation Failed');
+    //     callback(null, {
+    //         statusCode: 400,
+    //         headers: { 'Content-Type': 'text/plain' },
+    //         body: 'Couldn\'t create the todo item.',
+    //     });
+    //     return;
+    // }
+
+    // CREATE PARAMS TO SEARCH THE DB
+    const params = {
+        TableName: process.env.TODO_TABLE_NAME,
+        Key: {
+            id: event.pathParameters.id,
+        },
+    };
+
+    // GET THE TODO ITEM FROM DYNAMO
+    dynamoDb.get(params, (error, result) => {
+
+        // CHECK FOR ERRORS
+        if (error) {
+
+            console.error(error);
+
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Couldn\'t fetch the todo item.',
+            });
+            return;
+        }
+
+        // CREATE A RESPONSE TO SEND BACK WITH THE TODO ITEM
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Item),
+        };
+
+        callback(null, response);
+    });
+};
