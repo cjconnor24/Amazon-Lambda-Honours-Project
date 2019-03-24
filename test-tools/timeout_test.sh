2a1228a84c19dba482709d9749905c6ca5961018
Chris Connor <chris@chrisconnor.co.uk>
2019-03-24 20:05:53 +0000

    Set the maximum to 4 hours

--- test-tools/timeout_test.sh
+++ test-tools/timeout_test.sh
@@ -13,13 +13,14 @@ SLEEP_INTERVAL=5
 
 COUNT=0
 # bash until loop
-until [ $COUNT -gt 50 ]; do
+until [ $SLEEP_TIMER -gt 240 ]; do
+# while [ 1 ]; do
     
-    # curl -X $HTTP_METHOD -d "$DATA" -o /dev/null -s $BASE_URL -w "%{url_effective}\t${RUNTIME}\t%{http_code}\t%{time_pretransfer}\t%{time_starttransfer}\t%{time_total}\t$(echo $HTTP_METHOD)\t$(date)\n" | tee -a $RESULTS_FILENAME
+    curl -X $HTTP_METHOD -d "$DATA" -o /dev/null -s $BASE_URL -w "%{url_effective}\t${RUNTIME}\t%{http_code}\t%{time_pretransfer}\t%{time_starttransfer}\t%{time_total}\t$(echo $HTTP_METHOD)\t$(date)\n" | tee -a $RESULTS_FILENAME
     let COUNT=COUNT+1
     
     
-    echo "Sleeping for $SLEEP_TIMER minutes $(date +%H:%M:%S)"
+    echo "$RUNTIME: Sleeping for $SLEEP_TIMER minutes $(date +%H:%M:%S)"
     sleep $[$SLEEP_TIMER * 60]
     let SLEEP_TIMER=SLEEP_TIMER+SLEEP_INTERVAL
 done
