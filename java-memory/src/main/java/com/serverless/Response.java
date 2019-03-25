77387f173d7c55a58eb3b6917cc72a584bb937d7
Chris Connor <cjconnor24@connordesign.com>
2019-03-25 21:05:12 +0000

    Adding timestamp to body

--- java-memory/src/main/java/com/serverless/Response.java
+++ java-memory/src/main/java/com/serverless/Response.java
@@ -1,20 +1,25 @@
 package com.serverless;
 
+import java.util.Date;
 import java.util.Map;
 
 public class Response {
 
 	private final String message;
+	private final long timestamp;
 	private final Map<String, Object> input;
 
 	public Response(String message, Map<String, Object> input) {
 		this.message = message;
+		this.timestamp = new Date().getTime();
 		this.input = input;
 	}
 
 	public String getMessage() {
 		return this.message;
 	}
+
+	public long getTimestamp() {return this.timestamp;}
 
 	public Map<String, Object> getInput() {
 		return this.input;
