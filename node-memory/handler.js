e9217700d4dd0622db8dba49ad0fe81f4be63046
Chris Connor <cjconnor24@connordesign.com>
2019-03-25 20:48:19 +0000

    Updating the node function and serverless yaml to prepare for memory tests

--- node-memory/handler.js
+++ node-memory/handler.js
@@ -1,14 +1,12 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.memTest = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      timestamp: new Date().getTime(),
+      message: 'NodeJS Lambda Response. Memory Test. Function executed successfully!'
+      //input: event,
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
