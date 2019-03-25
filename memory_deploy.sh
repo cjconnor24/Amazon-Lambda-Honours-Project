da5dc399363e5df4a5bdd71a899a06833e737380
Chris Connor <cjconnor24@connordesign.com>
2019-03-25 21:12:56 +0000

    Adding scripts for the deployment and removal of memory functions

---
+++ memory_deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## declare an array variable
+declare -a arr=("java" "node" "python3")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   folder="$i-memory"
+   
+   cd $folder
+   
+   echo "Now deploying $i API"
+   echo ""
+   pwd
+   echo ""
+   echo "####################"
+
+   #sls deploy
+   
+
+   cd ../
+   # or do whatever with individual element of the array
+done
+
+echo ALL MEMORY FUNCTIONS HAVE BEEN DEPLOYED
+
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
