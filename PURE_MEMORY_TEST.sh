32ec8d1b22b9a419e69623827ad6875e2348fec5
Chris Connor <chris@chrisconnor.co.uk>
2019-04-16 07:23:45 +0100

    Properly commenting

--- PURE_MEMORY_TEST.sh
+++ PURE_MEMORY_TEST.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
-# CREATE ENTRIES
 
-# THIS SCRIPT WILL CREATE 200 ENTRIES AT AN INTERVAL OF 500ms
+# CHRIS CONNOR
+# S1715477
+# CCONNO208@CALEDONIAN.AC.UK
+
+# THIS TEST SCRIPT WILL RUN A SUITE OF COLD START TESTS AGAINST THE PURE
+# LAMBDA FUNCTIONS. TEST RESULTS WILL BE LOGGED ACROSS THE THREE MEMORY TIERS
+# AS OUTLINED IN THE EXECUTION SECTION OF THE REPORT. THE AIM OF THIS TEST IS
+# TO BYPASS API GATEWAY COMPLETELY TO COMPARE THE OVERHEADS
 
 
-# VARIABLES
-# BASE_URL="https://3u5d6296vf.execute-api.eu-west-2.amazonaws.com/node-dev/todos"
-# BASE_URL=$1
-DATA=$(jo text="Todo created at $(date)" checked=false)
+# COMMANDLINE INPUT PARAMETERS ./filename.sh [url] [runtime]
+# EXAMPLE USAGE: ./PURE_MEMORY_TEST.sh ./java-api JAVA
 RUNTIME=$2
 RUNTIME_FOLDER=$1
-# HTTP_METHOD=POST
+
+# FILENAME TO LOG RESULTS
 RESULTS_FILENAME="results_${RUNTIME}_PURE_MEMORY_${HTTP_METHOD}_$(date +%d-%m-%Y_%H%M).txt"
 
-# RUNTIME_API=${BASE_URL#*aws.com/}
-# echo $TEMP_URL
-
-# echo $RESULTS_FILENAME
-
-# -o /dev/null
-
+# MINUTES TO PAUSE FOR COLD START
 MINUTES=30
 
+# CHANGE DIRECTORY INTO THE SPECIFIED RUNTIME FOLDER
 cd $RUNTIME_FOLDER
 
+# CREATE COUNTER TO MANAGE ITERATIONS
 COUNT=0
-# bash until loop
+
+# LOOP FOR X
 until [ $COUNT -gt 500 ]; do
 
-    declare -a arr=("512" "1024" "2048")
+    declare -a MEMORY_TIERS=("512" "1024" "2048")
     
-    for i in "${arr[@]}"
+    # LOOP FOR EACH MEMORY TIER
+    for i in "${MEMORY_TIERS[@]}"
     do
-        # curl -X $HTTP_METHOD -d "$DATA" -o /dev/null -s $BASE_URL$i -w "%{url_effective}\t${RUNTIME}\t${i}\t%{http_code}\t%{time_pretransfer}\t%{time_starttransfer}\t%{time_total}\t$(echo $HTTP_METHOD)\t$(date)\n" | tee -a $RESULTS_FILENAME
-        
 
-        # SLEEP FOR 10s before running the next memory size
+        # INVOKE THE LAMBDA DIRECTLY BYPASSING API GATEWAY - PASS DUMMY DATA AND WRITE LOGS TO FILE
         sls invoke -f createTodo$i -l -p ../DUMMY_JSON_DATA.json | tee -a "${i}_${RESULTS_FILENAME}"
+
+        # WAIT 1 SECOND BEFORE CALLING THE NEXT MEMORY TIER
         sleep 1s
 
         # echo "$RUNTIME $i: Sleeping for $MINUTES minutes $(date +%H:%M:%S)"
     done
 
+    # INCREMENT THE COUNTER AND SLEEP FOR X
     let COUNT=COUNT+1
     sleep $[$MINUTES * 60]
-    # sleep 1s
+    
 done
